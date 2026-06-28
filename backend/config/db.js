3c3d83142c43382f1b59b1ee611882969199dc35
aloktiwari05 <aloktiwari0026@gmail.com>
2026-06-28 13:38:58 +0530

    Implement user signup endpoint with validation and error handling.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const db = new pg.Pool({
+const db = new pg.Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.HOST,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     database: 'todoapp'
 })
 
