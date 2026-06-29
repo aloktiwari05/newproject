90573e36d63a954ec8150ecffd46fb85174b6d2e
aloktiwari05 <aloktiwari0026@gmail.com>
2026-06-30 00:19:22 +0530

    hash user passwords with bcrypt during signup

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,7 @@
 import db from '../config/db.js'
+import bcrypt from 'bcrypt'
+
+const saltRounds = 12
 
 export const login = async (req, res) => {
     res.json({ message: "You are on login post route" })
@@ -11,7 +14,9 @@ export const signup = async (req, res) => {
         return res.status(400).json({ error: "Password should be minimum of 8 characters" })
     }
     try {
-        const response = await db.query('INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3)  RETURNING id, username, email', [formData.username, formData.email, formData.password])
+        const hash = await bcrypt.hash(formData.password, saltRounds)
+
+        const response = await db.query('INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3)  RETURNING id, username, email', [formData.username, formData.email, hash])
         res.status(201).json({ message: 'User Created Successfully', user: response.rows[0] })
     }
     catch (err) {
