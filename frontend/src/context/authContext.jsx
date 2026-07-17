e52c90ea0d798438a1591cbc0ce3a82024449079
aloktiwari05 <aloktiwari0026@gmail.com>
2026-07-17 23:27:58 +0530

    fix(auth): improve JWT refresh flow and error handling.

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -6,13 +6,14 @@ const AuthContext = createContext()
 const AuthProvider = ({ children }) => {
     const [accessToken, setAccessToken] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
+    const [user, setUser] = useState(null)
 
     useEffect(() => {
-        refresh(setAccessToken, setIsLoading);
+        refresh(setAccessToken, setIsLoading, setUser );
     }, [])
 
     return (
-        <AuthContext.Provider value={{ accessToken, setAccessToken, isLoading, setIsLoading }}>
+        <AuthContext.Provider value={{ accessToken, setAccessToken, isLoading, setIsLoading, user, setUser }}>
             {children}
         </AuthContext.Provider>
     )
