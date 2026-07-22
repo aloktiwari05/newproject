e40746313c6be46781834261dd13a98dc1d30818
aloktiwari05 <aloktiwari0026@gmail.com>
2026-07-22 23:17:08 +0530

    fix authflow bugs and update Login, Register page UI.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 // import React from 'react'
 // import Register from './components/Register.jsx'
+import { Outlet } from "react-router-dom"
+import Navbar from './components/Navbar.jsx'
+import Sidebar from "./components/Sidebar.jsx"
 
 function App() {
   return (
-    <div className='flex flex-col items-center'>
-      <div className='text-5xl text-green-300 text-center'>App</div>
-</div>
+    <div className='w-full h-full flex flex-col justify-center'>
+      <Navbar />
+      <div className="w-screen h-screen flex">
+        <Sidebar />
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='w-40 h-screen bg-yellow-200 sticky left-0 top-0'>Sidebar</div>
+  )
+}
+
+export default Sidebar
